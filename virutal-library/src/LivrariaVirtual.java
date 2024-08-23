c1ac02c5fc7de69ac30c84705805a84ff80c87b9
Isaac Conceição <60546623+isaac-conceicao@users.noreply.github.com>
2024-08-23 09:28:51 -0300

    create virtual-library app project
    add new project directory

---
+++ virutal-library/src/LivrariaVirtual.java
@@ -0,0 +1,31 @@
+public class LivrariaVirtual {
+    public static void main(String[] args) {
+        //@TODO
+    }
+
+    public void cadastrarLivro(){
+        //@TODO
+    }
+
+    public void realizarVenda(){
+        //@TODO
+    }
+
+    public void ListarLivrosImpressos(){
+        //@TODO
+    }
+
+    public void listarLivrosEletronicos(){
+        //@TODO
+    }
+
+    public void listarLivros(){
+        //@TODO
+    }
+
+    public void listarVendas(){
+        //@TODO
+    }
+
+
+}
