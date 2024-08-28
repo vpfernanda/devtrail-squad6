7de3ef354995aa191d41a72ed2038113b90c0a7e
vpfernanda <fernandavpagano@gmail.com>
2024-08-28 19:48:59 -0300

    "Implementação de Classes models de Pessoa e enum Sexo."

--- locadora-veiculos/src/main/java/devtrail/squad6/locadoraveiculos/model/entity/Pessoa.java
+++ locadora-veiculos/src/main/java/devtrail/squad6/locadoraveiculos/model/entity/Pessoa.java
@@ -1,4 +1,36 @@
 package devtrail.squad6.locadoraveiculos.model.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+
+@Entity
+@Table (name = "pessoa")
+@Inheritance(strategy = InheritanceType.JOINED)
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 128)
+    private String nome;
+
+    @Column(nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "sexo", nullable = false)
+    private Sexo sexo;
 }
