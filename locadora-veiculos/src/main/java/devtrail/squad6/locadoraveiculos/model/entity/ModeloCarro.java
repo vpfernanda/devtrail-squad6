794cc548b7709568b3f621b8278713b158f4349c
vpfernanda <fernandavpagano@gmail.com>
2024-08-30 08:46:27 -0300

    Implementando Serializable em ModeloCarro.

--- locadora-veiculos/src/main/java/devtrail/squad6/locadoraveiculos/model/entity/ModeloCarro.java
+++ locadora-veiculos/src/main/java/devtrail/squad6/locadoraveiculos/model/entity/ModeloCarro.java
@@ -6,12 +6,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class ModeloCarro {
+public class ModeloCarro implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
