641dd4b392b17a89dddd0670a5711132e3b2d4d9
devGabrielPedro <gabrielpedrodsantos@gmail.com>
2024-09-01 20:05:38 -0300

    alterações nos repositories de ModeloCarro e Motorista

--- locadora-veiculos/src/main/java/devtrail/squad6/locadoraveiculos/repository/MotoristaRepository.java
+++ locadora-veiculos/src/main/java/devtrail/squad6/locadoraveiculos/repository/MotoristaRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MotoristaRepository extends JpaRepository<Motorista, Long> {
+    Motorista findByEmail(String email);
+    Motorista findByCpf(String cpf);
+    Motorista findByNumeroCNH(String numeroCNH);
 }
